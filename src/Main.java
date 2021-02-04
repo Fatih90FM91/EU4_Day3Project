cec07cedf3f2b665ea3eadbfd0ebdf43bfe11481
HuntermanTeam <jonsnow9091.fs@gmail.com>
2021-02-04 14:38:48 +0200

    eu4 is good

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,8 @@ public class Main {
         public static void main(String[] args) {
             System.out.println("Hello Git Last day");
 
-            //EU4 is exited
+            //EU4 is excited
+            //EU4 is dedicated
 
         }
     }
